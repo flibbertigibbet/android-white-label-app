2233cf057943f3d799395834fb86743cfed4f1f0
Kathryn Killebrew <kkillebrew@azavea.com>
2014-07-11 18:06:43 -0400

    Test reading json from web into POJO and pretty-printing to view

--- app/src/main/java/com/votinginfoproject/VotingInformationProject/ContestsActivity.java
+++ app/src/main/java/com/votinginfoproject/VotingInformationProject/ContestsActivity.java
@@ -3,6 +3,7 @@ package com.votinginfoproject.VotingInformationProject;
 import android.app.Activity;
 import android.app.ActionBar;
 import android.app.Fragment;
+import android.os.AsyncTask;
 import android.os.Bundle;
 import android.view.LayoutInflater;
 import android.view.Menu;
@@ -10,10 +11,32 @@ import android.view.MenuItem;
 import android.view.View;
 import android.view.ViewGroup;
 import android.os.Build;
+import android.widget.Button;
+import android.widget.TextView;
 
+import java.io.BufferedInputStream;
+import java.io.BufferedReader;
+import java.io.IOException;
+import java.io.InputStream;
+import java.io.InputStreamReader;
+import java.io.Reader;
+import java.net.HttpURLConnection;
+import java.net.MalformedURLException;
+import java.net.URL;
+import java.security.Timestamp;
+import java.util.List;
+
+import com.google.gson.Gson;
+import com.google.gson.GsonBuilder;
 
 
 public class ContestsActivity extends Activity {
+
+    TextView testText;
+
+    public ContestsActivity() {
+
+    }
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
@@ -61,4 +84,95 @@ public class ContestsActivity extends Activity {
             return rootView;
         }
     }
+
+    public class ElevatorMeta {
+        Integer elevators_out;
+        String updated;
+    }
+
+    public class ElevatorOutageResult {
+        String line;
+        String station;
+        String elevator;
+        String message;
+        String message_html;
+        String alternate_url;
+    }
+
+    public class ElevatorOutages {
+        public ElevatorMeta meta;
+        public List<ElevatorOutageResult> results;
+    }
+
+    public void testDoStuff(View view) {
+        testText = (TextView) findViewById(R.id.testTextBox);
+        testText.setText("Hey, you clicked me!");
+
+        String testUrl = "https://raw.githubusercontent.com/flibbertigibbet/unlockphilly/master/testdata/elevator_outage_json_examples/outage5.json";
+        //String testGeoJson = "https://gist.githubusercontent.com/flibbertigibbet/9b2febda5f1aadf6add9/raw/989cfb5f62eec9001d433af3af24c77fa2b741c1/lancaster_ave.json";
+
+        new GetStuff().execute(testUrl);
+
+        /*
+        try(Reader reader = InputStreamReader (ContestsActivity.class.getResourceAsStream("location.json", "UTF-8")) {
+            Gson gson = new GsonBuilder().create();
+            TestThing myThing = gson.fromJson(reader, TestThing.class);
+            System.out.println(myThing);
+        }
+        */
+    }
+
+    private class GetStuff extends AsyncTask<String, Void, String> {
+
+        @Override
+        protected String doInBackground(String... urls) {
+            URL url = null;
+            String line;
+            StringBuilder gotStuff = new StringBuilder();
+            ElevatorOutages myOutages = null;
+            String show = "";
+
+            try {
+                url = new URL(urls[0]);
+                HttpURLConnection myConnection = (HttpURLConnection) url.openConnection();
+                InputStream in = myConnection.getInputStream();
+                //BufferedReader ir = new BufferedReader(new InputStreamReader(in));
+                InputStreamReader ir = new InputStreamReader(in);
+
+                Gson gson = new GsonBuilder().create();
+                myOutages = gson.fromJson(ir, ElevatorOutages.class);
+                System.out.println(myOutages);
+
+                System.out.println(myOutages.meta.elevators_out);
+                System.out.println(myOutages.meta.updated);
+
+                show = myOutages.meta.elevators_out.toString() + " elevators out:\n\n";
+                for (ElevatorOutageResult res : myOutages.results) {
+                    show += res.elevator + " - " + res.line + "\n" + res.message + "\n\n";
+                }
+
+                /*
+                while ((line = ir.readLine()) != null) {
+                    gotStuff.append(line);
+                    System.out.println(line);
+                }
+                */
+            } catch (MalformedURLException e) {
+                e.printStackTrace();
+                return "oops1";
+            } catch (IOException e) {
+                e.printStackTrace();
+                return "oops2";
+            }
+
+            return show;
+            //return gotStuff.toString();
+        }
+
+        protected void onPostExecute(String show) {
+            testText.setText(show);
+        }
+    }
+
 }
+

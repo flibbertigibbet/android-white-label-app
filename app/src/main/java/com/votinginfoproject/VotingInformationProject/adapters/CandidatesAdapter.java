103e4832c81f33e9bfa846e66bb0f9f987861678
Kathryn Killebrew <kkillebrew@azavea.com>
2014-08-13 12:50:59 -0400

    Use LRU cache for candidate photos

--- app/src/main/java/com/votinginfoproject/VotingInformationProject/adapters/CandidatesAdapter.java
+++ app/src/main/java/com/votinginfoproject/VotingInformationProject/adapters/CandidatesAdapter.java
@@ -1,5 +1,6 @@
 package com.votinginfoproject.VotingInformationProject.adapters;
 
+import android.graphics.Bitmap;
 import android.util.Log;
 import android.view.LayoutInflater;
 import android.view.View;
@@ -76,8 +77,9 @@ public class CandidatesAdapter extends ArrayAdapter<Candidate> {
         setTextView(viewHolder.name, candidate.name);
         setTextView(viewHolder.party, candidate.party);
 
-        if (candidate.photo != null) {
-            viewHolder.photo.setImageBitmap(candidate.photo);
+        Bitmap havePhoto = candidate.getCandidatePhoto();
+        if (havePhoto != null) {
+            viewHolder.photo.setImageBitmap(havePhoto);
         } else if (candidate.photoUrl != null && !candidate.photoUrl.isEmpty() && !viewHolder.alreadyQueryingForPhoto) {
             viewHolder.alreadyQueryingForPhoto = true;
             new FetchImageQuery(candidate, viewHolder.photo).execute(candidate.photoUrl);
